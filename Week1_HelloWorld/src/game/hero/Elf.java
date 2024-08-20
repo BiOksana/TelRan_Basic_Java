12c5458831fc0c44ca1aee5f1983e88adfa76e97
BiOksana <biokissme@gmail.com>
2024-08-20 20:45:15 +0200

    Created package game

---
+++ Week1_HelloWorld/src/game/hero/Elf.java
@@ -0,0 +1,51 @@
+package game.hero;
+
+public class Elf {
+
+    int id;
+    String name;
+    int age;
+    double height;
+    boolean isKind;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public boolean isKind() {
+        return isKind;
+    }
+
+    public void setKind(boolean kind) {
+        isKind = kind;
+    }
+
+}
