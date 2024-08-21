d9a8e5477709fd810b7678375940c20875b70d65
BiOksana <biokissme@gmail.com>
2024-08-21 18:28:35 +0200

    Created class Dog

---
+++ Week1_HelloWorld/src/homeWork3/Dog.java
@@ -0,0 +1,25 @@
+package homeWork3;
+
+public class Dog {
+    String name;
+    String breed;
+    static int totalDogs;
+
+    public Dog(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+        totalDogs++;
+    }
+
+    public void printDogInfo() {
+        System.out.println("Dog's name: " + name + ", dog's breed: " + breed);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Sharik", "mops");
+        dog.printDogInfo();
+        System.out.println(Dog.totalDogs);
+    }
+}
