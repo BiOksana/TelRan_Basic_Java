508f8d7bdcf1395b6b68b30209e34a0a984ea02a
BiOksana <biokissme@gmail.com>
2024-09-04 08:57:07 +0200

    Changed homework 5

--- week2/src/eighthLesson/homeWork5/First.java
+++ week2/src/eighthLesson/homeWork5/First.java
@@ -2,14 +2,16 @@ package eighthLesson.homeWork5;
 
 public class First {
     public static void main(String[] args) {
-    double m = 10.5;
-    double n = 10.45;
+        double m = 10.5;
+        double n = 10.45;
 
 
-    if (Math.abs(n - 10) < Math.abs(m - 10)) {
-        System.out.println("Число n = " + n + " ближе к 10, чем число m = " + m);
-    } else {
-        System.out.println("Число m = " + m + " ближе к 10, чем число n = " + n);
-    }
+        if (Math.abs(n - 10) < Math.abs(m - 10)) {
+            System.out.println("Число n = " + n + " ближе к 10, чем число m = " + m);
+        } else if (Math.abs(m - 10) < Math.abs(n - 10)) {
+            System.out.println("Число m = " + m + " ближе к 10, чем число n = " + n);
+        } else {
+            System.out.println("Oба числа равноудалены");
+        }
     }
 }
